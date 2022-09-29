fa5754809a80febdd77c57153ab9c13378a5b06f
JakeJR0 <1453194@stu.tmc.ac.uk>
2022-09-29 19:15:56 +0100

    Adding multiplication table

--- index.php
+++ index.php
@@ -11,14 +11,14 @@ include 'assets/php/calculations.php';
         <meta name="description" content="<?php echo SITE_DESCRIPTION; ?>">
         <title><?php echo SITE_NAME; ?></title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+        <link rel="stylesheet" href="assets/css/styles.css">
     </head>
     <body>
-        <script src="assets/js/calculator.js"></script>
-
         <div class="text-center" style="margin-top: 50px;">
             <h1 class="display-4"><?php echo SITE_NAME; ?></h1>
             <lead><?php echo SITE_DESCRIPTION; ?></lead>
         </div>
+        
         <div class="container" style="margin-top: 50px;">
             <div class="row">
                 <row></row>
@@ -27,12 +27,14 @@ include 'assets/php/calculations.php';
 
             if (isset($_POST["number_1"])) {
                 $number_1 = $_POST["number_1"];
+                unset($_POST["number_1"]);
             } else {
                 $number_1 = null;
             }
 
             if (isset($_POST["number_2"])) {
                 $number_2 = $_POST["number_2"];
+                unset($_POST["number_2"]);
             } else {
                 $number_2 = null;
             }
@@ -40,6 +42,7 @@ include 'assets/php/calculations.php';
             if (isset($_POST["operator"])) {
                 $operator = $_POST["operator"];
                 $operator = strtolower($operator);
+                unset($_POST["operator"]);
             } else {
                 $operator = null;
             }
@@ -71,17 +74,43 @@ include 'assets/php/calculations.php';
                 </div>
                 <div class="text-center" style="margin-top: 20px;">
                     <button type="submit" class="btn btn-primary">Submit</button>
-                    <button class="btn btn-danger" onclick="resetCalculator()">Reset</button>
                 </div>
                 
                 <div class="text-center" style="margin-top: 20px;">
                     <div class="mb-3">
                         <label class="form-label">Result</label>
-                        <input class="form-control text-center" id="result" readonly type="text" value="<?php echo calculate($number_1, $number_2, $operator); ?>">
+                        <input class="form-control text-center" id="result" readonly type="text" placeholder="Empty" value="<?php echo calculate($number_1, $number_2, $operator); ?>">
                     </div>
                 </div>
             </form>
             </div>
+        </div>
+        <div class="table-holder">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col" colspan=10>Mutiplication Table</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    for ($i = 1; $i <= 10; $i++) {
+                        echo '<tr>';
+                        for ($j = 1; $j <= 10; $j++) {
+                            $cell =  $i * $j;
+                            if ($i > 0 && $i <= 10 && $j == 1 || $i == 1) {
+                                echo '<th>'.$cell.'</th>';
+                                
+                            } else {
+                                echo '<td>'.$cell.'</td>';
+                            }
+                            
+                        }
+                        echo '</tr>';
+                    }
+                    ?>
+                </tbody>
+            </table>
         </div>
     </body>
 </html>
